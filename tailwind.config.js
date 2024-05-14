2fd8c62bcfdaf8bfb0e6cd3119a5cacdcdb5d6ca
coders-rabbi <pcrabbi2020@gmail.com>
2024-05-15 05:17:14 +0600

    default font changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extends: ["eslint:recommended", "plugin:react/recommended"],
+    fontFamily: {
+      body: ["Playfair Display", "serif"],
+    },
   },
   // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
