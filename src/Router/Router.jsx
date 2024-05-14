5a1f99c7dca8419240fa7c1413038e8c203c40df
coders-rabbi <pcrabbi2020@gmail.com>
2024-05-15 04:53:51 +0600

    navbar navigation almost done!

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,17 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layout/Root";
 import Home from "../pages/home/Home";
+import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    children:[
-        {
-            path: "/",
-            element: <Home></Home>
-        }
-    ]
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
--- src/pages/sharedComponents/navBar/NavBar.jsx
+++ src/pages/sharedComponents/navBar/NavBar.jsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+
 const NavBar = () => {
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link>Room</Link>
+      </li>
+      <li>
+        <Link to="about">About</Link>
+      </li>
+      <li>
+        <Link to="contact">Contact</Link>
+      </li>
+    </>
+  );
+
   return (
-    <div className="navbar absolute bg-black opacity-75">
+    <div className="navbar absolute">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -21,40 +41,18 @@ const NavBar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white"
           >
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navItems}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">
+          <img className="w-28" src={logo} alt="" />
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a href="">Rooms</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
+        <ul className="menu menu-horizontal px-1 text-white text-2xl ">
+          {navItems}
         </ul>
       </div>
       <div className="navbar-end">
