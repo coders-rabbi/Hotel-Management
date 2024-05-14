da5931f75712577e0419ca766572a250d46dc7ea
coders-rabbi <pcrabbi2020@gmail.com>
2024-05-14 13:43:30 +0600

    middle banner almost done!

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import NavBar from "../sharedComponents/navBar/NavBar";
 import Accomodation from "./Accomodation";
 import DisplayRoom from "./DisplayRoom";
 import HeroSection from "./HeroSection";
+import MiddleBanner from "./MiddleBanner";
 import Services from "./Services";
 // import TopHeader from "./TopHeader";
 
@@ -14,6 +15,7 @@ const Home = () => {
       <Accomodation />
       <DisplayRoom />
       <Services />
+      <MiddleBanner/>
     </>
   );
 };
---
+++ src/pages/home/MiddleBanner.jsx
@@ -0,0 +1,21 @@
+import "./BackgroundImage.css";
+
+const MiddleBanner = () => {
+  return (
+    <div className="middle-banner mt-24">
+      <div className="max-w-screen-lg m-auto">
+        <div className="bg-01 w-[700px] py-20">
+          <h3 className="uppercase text-center text-5xl text-white font-semibold mb-10">
+            Sanification
+          </h3>
+          <h1 className="text-center text-white text-8xl mb-5">
+            20 <span className="text-5xl">%</span>
+          </h1>
+          <p className="uppercase text-center text-xl text-white">winter best promo price</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MiddleBanner;
